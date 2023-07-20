d8bc38eb2d1a9e5888c5f2bbcf765c34055047b6
priyankanair1 <priyankanairammu90@gmail.com>
2023-07-20 11:53:28 -0700

    Delete Activity, Edit Activity, Display Priorities

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,95 +1,131 @@
-const Activity = require('../models/activity');
-const moment = require('moment');
+const Activity = require("../models/activity");
+const moment = require("moment");
 
 module.exports = {
-    index,
-    add,
-    create
-  };
+  index,
+  add,
+  create,
+  delete: deleteActivity,
+  edit,
+  update,
+};
 
-
-  async function index(req, res) {
-    try {
-      let activitydate = req.query.date;
-      if(!activitydate) {
-        activitydate = new Date().toLocaleDateString('fr-CA', { year: 'numeric', month: '2-digit', day: '2-digit' });
-      }
-      const activityday = moment(activitydate, 'YYYY-MM-DD').startOf('day')
-      console.log(activityday);
-      
-      const activities = await Activity.find({
-        activitytime: {
-          $gte: activityday.toDate(),
-          $lte: moment(activityday).endOf('day').toDate()
-        },
-        user: req.user.id
-      })
-
-      //const activities = await Activity.find({});
-      res.render('activities/index', { title: 'My Activities', activities, activitydate });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  function add(req, res) {
-     res.render('activities/add', { title: 'Add Activity', errorMsg: "" });
-  }
-
-  async function create(req, res) {
-    try {
-      //req.body.user = req.user.id;
-      let activity = new Activity({
-        activity: req.body.activity,
-        activitytime: req.body.activitytime,
-        user: req.user.id,
-        priority: req.body.priority,
-        notes: req.body.notes,
+async function index(req, res) {
+  try {
+    let activitydate = req.query.date;
+    if (!activitydate) {
+      activitydate = new Date().toLocaleDateString("fr-CA", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
       });
-
-      await Activity.create(activity);
-      res.redirect("/activities");
-    } catch (err) {
-      // Typically some sort of validation error
-      console.log(err);
-      res.render("activities/add", { title: 'Add Activity', errorMsg: err.message });
     }
+    const activityday = moment(activitydate, "YYYY-MM-DD").startOf("day");
+
+    const activities = await Activity.find({
+      activitytime: {
+        $gte: activityday.toDate(),
+        $lte: moment(activityday).endOf("day").toDate(),
+      },
+      user: req.user.id,
+    });
+
+    const priorities = activities.filter(a => a.priority);
+    
+    res.render("activities/index", {
+      title: "My Activities",
+      activities,
+      activitydate,
+      priorities,
+      errorMsg: "",
+    });
+  } catch (err) {
+    console.log(err);
   }
+}
 
+function add(req, res) {
+  let activitydate = new Date().toTimeString();
+  console.log(activitydate);
+  res.render("activities/add", {
+    title: "Add Activity",
+    activitydate,
+    errorMsg: "",
+  });
+}
 
-/*
+async function create(req, res) {
+  try {
 
-  async function create(req, res) {
-    try {
-      req.body.user = req.user.id;
-      // let activity = new Activity({
-      //   activity: req.body.activity,
-      //   activitytime: req.body.activitytime,
-      //   user: req.body.user.id,
-      //   priority: req.body.priority
-      // });
+    console.log("req.body");    
+    console.log(req.body);
+    console.log("req.body.priority");
+    console.log(req.body.priority);
+    let activity = new Activity({
+      activity: req.body.activity,
+      activitytime: req.body.activitytime,
+      user: req.user.id,
+      priority: req.body.priority,
+      notes: req.body.notes,
+    });
 
-      // console.log("activityyyyyy");
-      // console.log(activity);
-      // console.log("bodyyyyyyy");
-      // console.log(req.body);
+    await Activity.create(activity);
+    res.redirect("/activities");
+  } catch (err) {
+    // Typically some sort of validation error
+    console.log(err);
+    res.render("activities/add", {
+      title: "Add Activity",
+      errorMsg: err.message,
+    });
+  }
+}
 
-      // let activityObj = await Activity.create(req.body);
-     /* console.log(activityObj);
-      console.log(activityObj);
+async function deleteActivity(req, res) {
+  console.log("delete");
+  try {
+    const activity = await Activity.findOne({ _id: req.params.id });
+    await Activity.deleteOne(activity);
+    res.redirect("/activities");
+  } catch (error) {
+    console.log(error.message);
+    res.render("activities", {
+      title: "Delete Activity",
+      errorMsg: err.message,
+    });
+  }
+}
 
-      req.body.notes.forEach(noteEl => {
-        let noteObj = new Note({
-          note: noteEl
-        });
-        activityObj.notes.push(noteObj);
-        activityObj.save();            
-      });
-     await Activity.create(req.body);
-     res.redirect("/activities");
-    } catch (err) {
-      // Typically some sort of validation error
-      console.log(err);
-      res.render("activities/add", { title: 'Add Activity', errorMsg: err.message });
-    }
-  }*/
+async function edit(req, res) {
+  console.log("edit");
+  let activitydate = new Date().toLocaleDateString("fr-CA", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  const activity = await Activity.findOne({ _id: req.params.id });
+  res.render("activities/edit", {
+    title: "Edit Activity",
+    errorMsg: "",
+    activity,
+    activitydate,
+  });
+}
+
+async function update(req, res) {
+  try {
+    const updateUser = await Activity.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    res.redirect("/activities");
+  } catch (error) {
+    console.log(error.message);
+    res.render("activities/edit", {
+      title: "Edit Activity",
+      errorMsg: err.message,
+    });
+  }
+}
--- routes/activities.js
+++ routes/activities.js
@@ -6,6 +6,15 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 
 router.get('/', ensureLoggedIn, activityCtrl.index);
+
 router.get('/add',ensureLoggedIn, activityCtrl.add);
+
 router.post('/',ensureLoggedIn, activityCtrl.create);
+
+router.delete('/:id',ensureLoggedIn, activityCtrl.delete);
+
+router.get('/:id/edit',ensureLoggedIn, activityCtrl.edit);
+
+router.put('/:id',ensureLoggedIn, activityCtrl.update);
+
 module.exports = router;
